d5d6ea68d7ae7e8a4298dae4e688d1db3d1f867e
SeymurSm <seymur.sm@gmail.com>
2024-04-23 00:32:39 +0400

    Car click mechanic implemented

---
+++ Assets/Scripts/SwapImage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SwapImage : MonoBehaviour
+{
+    private Card m_card = null;
+    private void Awake()
+    {
+        m_card = GetComponentInParent<Card>();
+    }
+    public void SwapCardImage()
+    {
+        m_card.SwapImage();
+    }
+}
